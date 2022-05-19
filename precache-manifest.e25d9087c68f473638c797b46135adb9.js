94919153de002ad92968cae4e7eaa525c96c8c52
Hamza Iftikhar <h.m89012@gmail.com>
2022-05-19 23:06:21 +0500

    Base Build

---
+++ precache-manifest.e25d9087c68f473638c797b46135adb9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6af37eef9fae2a5e8215c8c0d0b10fc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be5359cc943f75239585",
+    "url": "/static/css/main.8313b0cc.chunk.css"
+  },
+  {
+    "revision": "6e7caba2143c013b5e9e",
+    "url": "/static/js/2.2487f5aa.chunk.js"
+  },
+  {
+    "revision": "d24f9a0476df23dc7be232de3215b628",
+    "url": "/static/js/2.2487f5aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8565ebb4a33db69b2571",
+    "url": "/static/js/3.d6cc70fa.chunk.js"
+  },
+  {
+    "revision": "be5359cc943f75239585",
+    "url": "/static/js/main.ef933fe4.chunk.js"
+  },
+  {
+    "revision": "1fe92e7c25f04e541c2f",
+    "url": "/static/js/runtime-main.7081d51b.js"
+  },
+  {
+    "revision": "f45e90e0f9395282abaf753563b414e7",
+    "url": "/static/media/background.f45e90e0.png"
+  }
+]);
