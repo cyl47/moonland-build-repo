7758e4020af67da7f6ddc1043b979e16fa24bd9d
Hamza Iftikhar <h.m89012@gmail.com>
2022-08-20 10:38:29 +0500

    Updated React Build
    Solved Voice Issue with wallet

---
+++ public/static/js/13.bfb8a316.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmoonland=this.webpackJsonpmoonland||[]).push([[13,18],{1636:function(n,o){},1639:function(n,o){},1656:function(n,o){}}]);
+//# sourceMappingURL=13.bfb8a316.chunk.js.map
