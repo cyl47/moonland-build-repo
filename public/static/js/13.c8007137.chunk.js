d1642f1f3b3be4bcceff3433cfcacc954fd8c880
Hamza <h.m89012@gmail.com>
2023-02-16 10:39:34 +0500

    Revert "Updated React Build."
    This reverts commit d461bdfaffea5f1431b30ba97ccdaec9bda4f3be.

---
+++ public/static/js/13.c8007137.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmoonland=this.webpackJsonpmoonland||[]).push([[13,18],{1672:function(n,o){},1675:function(n,o){},1692:function(n,o){}}]);
+//# sourceMappingURL=13.c8007137.chunk.js.map
